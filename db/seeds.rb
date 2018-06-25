051e6f42ed94bccfd6b531f8b5722fe97c758c8f
Michael Johnson <micjohns@localhost.localdomain>
2018-06-25 15:39:24 -0400

    Ruby on Rails Tutorial

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+# . . .
+  Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+  description:
+    %{<p>
+      Ruby is the fastest growing and most exciting dynamic language
+      out there. If you need to get working programs delivered fast,
+      you should add Ruby to your toolbox.
+    </p>},
+    image_url: 'https://rebornix.gallerycdn.vsassets.io/extensions/rebornix/ruby/0.18.0/1523397294573/Microsoft.VisualStudio.Services.Icons.Default',
+    price: 49.95)
+# . . .
